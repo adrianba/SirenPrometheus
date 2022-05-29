6850f676dcc35d1f36f32a787984a11c79f7a7f8
Ade Bateman <bateman@acm.org>
2022-05-29 22:24:20 +0100

    Fill out remaining data.

--- src/boatdata.ts
+++ src/boatdata.ts
@@ -11,15 +11,31 @@ const temperature_galley = new Gauge({
   name: "temperature_galley",
   help: "Galley Temperature",
 });
-
 const temperature_fridge = new Gauge({
   name: "temperature_fridge",
   help: "Fridge Temperature",
 });
-
 const temperature_engine = new Gauge({
   name: "temperature_engine",
   help: "Fridge Temperature",
+});
+
+const battery_voltage1 = new Gauge({
+  name: "battery_voltage1",
+  help: "Battery 1 Voltage",
+});
+const battery_voltage2 = new Gauge({
+  name: "battery_voltage2",
+  help: "Battery 2 Voltage",
+});
+
+const is_shorepower_connected = new Gauge({
+  name: "is_shorepower_connected",
+  help: "Shore Power Connected",
+});
+const is_bilgepump_running = new Gauge({
+  name: "is_bilgepump_running",
+  help: "Bilge Pump Running",
 });
 
 setInterval(() => {
@@ -43,6 +59,13 @@ async function setData() {
   temperature_engine.set(
     round(wireless.items[1].data.internalTemperature.current)
   );
+
+  battery_voltage1.set(round(vessel.battery1.voltage.current));
+  battery_voltage2.set(round(vessel.battery2.voltage.current));
+
+  is_shorepower_connected.set(vessel.shorePower.isConnected ? 1 : 0);
+  is_bilgepump_running.set(vessel.bilge1.state ? 1 : 0);
+
   console.log("Updated data.");
 }
 
